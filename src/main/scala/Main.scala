c1a5689d53d753658f57f0e8c8b18fa56b4a2c28
salma fayez <salma.fayez.himida@gmail.com>
2022-12-04 16:26:09 +0200

    add notification serializer and deserializer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,13 @@ import zhttp.http.Body
 import zhttp.http.Method.POST
 import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
 import zio._
+import zio.json.{DecoderOps, EncoderOps}
+import zio.kafka.consumer.Consumer
+import zio.kafka.serde.Serde
 object Main extends ZIOAppDefault {
+
+
+
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
 
@@ -13,16 +19,24 @@ object Main extends ZIOAppDefault {
       )
     } yield println(req.status)
      s.provide(EventLoopGroup.auto(10),ChannelFactory.auto)
-    //
-    //    NotificationConsumer
-    //      .stream
-    //      .groupedWithin(100000, 1.minute)
-    //      .tap(chunk => ZIO.log(chunk.size.toString))
-    //      .flattenChunks
-    //      .map(_.offset)
-    //      .aggregateAsync(Consumer.offsetBatches)
-    //      .mapZIO(_.commit)
-    //      .runDrain
 
+        NotificationConsumer
+          .stream
+          .tap(r => Console.printLine(r.value))
+          .groupedWithin(100000, 1.minute)
+          .tap(chunk => ZIO.log(chunk.size.toString))
+//          .tap(m => {
+//            Client.request(url = "http://localhost:3000/notification",
+//              method = POST,
+//              content = Body.fromString(m.toString())
+//            )
+//          })
+          .flattenChunks
+          .map(_.offset)
+          .aggregateAsync(Consumer.offsetBatches)
+          .mapZIO(_.commit)
+          .runDrain
+          .provide(EventLoopGroup.auto(10),ChannelFactory.auto)
+//
   }
 }
--- src/main/scala/NotificationConsumer.scala
+++ src/main/scala/NotificationConsumer.scala
@@ -1,4 +1,6 @@
-import zio.ZLayer
+import com.fasterxml.jackson.annotation.JsonProperty
+import zio.{ZIO, ZLayer}
+import zio.json._
 import zio.kafka.consumer.Consumer.{AutoOffsetStrategy, OffsetRetrieval}
 import zio.kafka.consumer._
 import zio.kafka.serde.Serde
@@ -6,24 +8,51 @@ import zio.stream.ZStream
 
 object NotificationConsumer {
 
-  private val BOOSTRAP_SERVERS = List("localhost:9093")
+  private val BOOSTRAP_SERVERS = List("localhost:9092")
 
   private val consumer: ZLayer[Any, Throwable, Consumer] =
     ZLayer.scoped(
       Consumer.make(
         ConsumerSettings(BOOSTRAP_SERVERS)
-          .withGroupId("streaming-kafka")
+          .withGroupId("streaming-k")
           .withOffsetRetrieval(OffsetRetrieval.Auto(AutoOffsetStrategy.Earliest))
       )
     )
 
-  private def consume(): ZStream[Consumer, Throwable, CommittableRecord[String,String]] = {
-    val KAFKA_TOPIC = "subscriber-msg-million"
+  private def consume(): ZStream[Consumer, Throwable, CommittableRecord[String, Notification]] = {
+    val KAFKA_TOPIC = "notification"
     Consumer
       .subscribeAnd(Subscription.topics(KAFKA_TOPIC))
-      .plainStream(Serde.string, Serde.string)
+      .plainStream(Serde.string, Notification.notificationSerde)
   }
 
-  val stream: ZStream[Any, Throwable, CommittableRecord[String,String]] = consume().provideLayer(consumer)
+  val stream: ZStream[Any, Throwable, CommittableRecord[String, Notification]] =
+    consume().provideLayer(consumer)
 
 }
+
+case class Notification(
+   channel: String,
+   template: String,
+   segments: String,
+   subscribe: String,
+   name: String,
+   @JsonProperty("promotion_status")
+   promotionStatus: String,
+   @JsonProperty("payment_type")
+   paymentType: String
+)
+
+private object Notification {
+  implicit val encoder: JsonEncoder[Notification] = DeriveJsonEncoder.gen[Notification]
+  implicit val decoder: JsonDecoder[Notification] = DeriveJsonDecoder.gen[Notification]
+
+
+  val notificationSerde: Serde[Any, Notification] = Serde.string.inmapM { string =>
+    ZIO.fromEither(string.fromJson[Notification].left.map(errorMessage => new RuntimeException(errorMessage)))
+    } { notification => ZIO.succeed(notification.toJson)
+  }
+
+}
+
+
